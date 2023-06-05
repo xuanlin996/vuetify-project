8b6d99ecbc5b090a5d91087cd58bcd7fc1ec5c2b
xuanlin996 <xuan.lin129@gmail.com>
2023-06-05 15:57:15 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+// Composables
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('@/views/ViewHome.vue'),
+    meta: {
+      title: '國立科技高中─校園社團介紹網'
+    }
+  },
+  {
+    path: '/guitar',
+    component: () => import('@/views/ViewGuitar.vue'),
+    meta: {
+      title: '吉他社'
+    }
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes
+})
+
+router.afterEach((to, from) => {
+  document.title = to.meta.title
+})
+
+export default router
